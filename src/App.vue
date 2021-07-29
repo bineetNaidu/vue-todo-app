18a054e13bcdb6e1bf540bf2c31720b605292bbf
bineetNaidu <bineet.s.naidu@gmail.com>
2021-07-29 15:58:06 +0530

    🚀 the complete app

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,35 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div class="container">
+		<form class="formContainer" @submit="handleSubmit">
+			<input class="input" type="text" placeholder="Enter Your Todo" v-model="text" />
+			<button type="submit">Add</button>
+		</form>
+		<h3>Your Todos:</h3>
+		<ul class="todoItem">
+			<li v-for="todo in todos" :key="todo">{{todo}}</li>
+		</ul>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+	data () {
+		return {
+			text: '',
+			todos: [],
+			handleSubmit(e){
+				// this will prevent reloading the page on form submittion
+				e.preventDefault();
+				// this will check if the text is empty
+				if(!this.text) return;
+				// finally we add the todo to the todos array and set the text to empty
+				this.todos.push(this.text);
+				this.text = '';
+			}
+		}
+	},
 }
 </script>
 
@@ -23,4 +42,51 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+.container{
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-direction: column;
+	width: 250px;
+	margin: 0 auto;
+}
+
+.formContainer {
+	margin-bottom: 20px;
+	display: flex;
+	justify-content: space-between;
+}
+
+.formContainer input {
+	width: 100%;
+	padding: 12px 20px;
+	border: 1px solid #ccc;
+	border-radius: 4px;
+	box-sizing: border-box;
+	font-size: 1.2em;
+}
+
+.formContainer button {
+	display: none;
+}
+
+.todoItem{
+	width: 100%;
+	list-style: none;
+	margin: 0 auto;
+	padding: 0;
+	border-radius: 4px;
+	background-color: #f5f5f5;
+}
+
+.todoItem li{
+	width: 100%;
+	margin: 2px auto;
+	padding: 8px 0px;
+	border: 1px solid #e0e0e0;
+	border-radius: 4px;
+	background-color: #f5f5f5;
+}
+
 </style>
